2b7797c1a41482cfad6bf186a657460eaf2bce67
Adam <adamjamesgray.1992@gmail.com>
2021-08-08 22:25:59 +0100

    Add initial layouts and pages

--- app.js
+++ app.js
@@ -3,6 +3,16 @@ const express = require('express');
 const app = express();
 const PORT = 3055;
 
+app.set('view engine', 'pug');
+app.set('views', 'views');
+
+app.get('/', (req, res, next) => {
+  const payload = {
+    title: 'Twitter - Home',
+  };
+  res.status(200).render('home', payload);
+});
+
 const server = app.listen(PORT, () => {
-  console.log('Server running...');
+  console.log(`Server running on port ${PORT}...`);
 });
